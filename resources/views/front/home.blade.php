42d543ec60eceac461cd74bf61db27880f8318b8
nikhilgoku8 <nikhilgoku8@gmail.com>
2025-06-25 23:57:24 +0530

    Homepage Start

--- resources/views/front/home.blade.php
+++ resources/views/front/home.blade.php
@@ -2,10 +2,72 @@
 
 @section('content')
 
-<div class="banner">
-    <div class="main_banner owl-carousel owl-theme">    
-        <div class="item_box">
-            <img src="images/banners/banner-1.jpg" alt="" title="">
+<div class="hero">
+    <div class="video_wrapper">
+        <video autoplay muted loop>
+            <source src="{{ asset('front/assets/videos/OEC.mp4') }}" type="video/mp4">
+        </video>
+        <div class="overlay_text">LEADING MANUFACTURER OF <br> ELECTRICAL AND AUTOMOTIVE SOLUTIONS</div>
+    </div>
+</div>
+
+<div class="about">
+    <div class="container">
+        <div class="inner_container">
+            
+            <div class="heading">The Complete Manufacturer</div>
+
+            <div class="inner_box">
+                <div class="col-sm-6">
+                    <div class="text_box">
+                        <div class="title">About OEC</div>
+                        <p>OEC was established in 1974 and is a family-owned, global leader in the manufacturing of electrical connectors, fittings, cable glands, automotive fittings, air brake tubing, and hoses. Over the last 50 years, OEC has consistently expanded its product portfolio to cater to the diverse needs of the electrical and automotive industries across the globe. </p>
+                        <a href="#" class="hollow_btn">Read More</a>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <div class="rings_wrapper">
+                        <div class="ring_box">
+                            <div class="ring">
+                                <span class="lil_big">700,000</span>
+                                <span class="lil_small">SQ.FT of Manufacturing</span>
+                            </div>
+                        </div>
+                        <div class="ring_box">
+                            <div class="ring">
+                                <span class="lil_big">15,000 +</span>
+                                <span class="lil_small">SKU’s</span>
+                            </div>
+                        </div>
+                        <div class="ring_box">
+                            <div class="ring">
+                                <span class="lil_big">100 +</span>
+                                <span class="lil_small">Countries</span>
+                            </div>
+                        </div>
+                        <div class="ring_box">
+                            <div class="ring">
+                                <span class="lil_big">2,100 +</span>
+                                <span class="lil_small">Employees</span>
+                            </div>
+                        </div>
+                        <div class="ring_box">
+                            <div class="ring">
+                                <span class="lil_big">5</span>
+                                <span class="lil_small">Facilities</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="clr"></div>
+            </div>
+
+            <div class="video_wrapper">
+                <video autoplay muted loop>
+                    <source src="{{ asset('front/assets/videos/about.mp4') }}" type="video/mp4">
+                </video>
+            </div>
+
         </div>
     </div>
 </div>
--- resources/views/front/layout/header.blade.php
+++ resources/views/front/layout/header.blade.php
@@ -15,7 +15,13 @@
 
 <!---fonts-->
 <link href="{{ asset('front/assets/css/fontawesome-5.15.3.css') }}" rel="stylesheet" type="text/css" />
-<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet">
+<!-- <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600;700&display=swap" rel="stylesheet"> -->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<!-- <link href="https://fonts.googleapis.com/css2?family=Michroma&display=swap" rel="stylesheet"> -->
+<link href="https://fonts.googleapis.com/css2?family=Michroma&family=Open+Sans:ital,wght@0,300..800;1,300..800&family=Roboto+Flex:opsz,wght@8..144,100..1000&display=swap" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
 
 <!---menu-->
 <!-- <link rel="stylesheet" href="{{ asset('front/assets/css/menu-style.css') }}" type="text/css" media="all" /> -->
@@ -39,38 +45,62 @@
 <header>  
 
 <div class="container">
-    <div class="logo">
-        <a href="{{ url('/') }}"><img src="{{ asset('front/assets/images/logo.webp') }}" alt="" /></a>
-    </div>
-    <nav>
-        <!-- Menu Toggle btn-->
-        <div class="menu-toggle">
-            <h3>Menu</h3>
-            <button type="button" id="menu-btn">
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-                <span class="icon-bar"></span>
-            </button>
+    <div class="inner_container">
+        <div class="logo">
+            <a href="{{ route('home') }}"><img src="{{ asset('front/assets/images/logo.webp') }}" alt="" /></a>
         </div>
-        <!-- Responsive Menu Structure-->
-        <!--Note: declare the Menu style in the data-menu-style="horizontal" (options: horizontal, vertical, accordion) -->
-        <ul id="respMenu" class="ace-responsive-menu" data-menu-style="horizontal">
-            <li>
-                <a>
-                    <span class="title">About</span>
-                    <span class="arrow"></span> 
-                </a>
-                <ul>
-                    <li><a href="#">Lorem Ipsum 1</a></li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">
-                    <span class="title">Lorem Ipsum 2</span>
-                </a>
-            </li>
-        </ul>
-    </nav>
+        <nav>
+            <!-- Menu Toggle btn-->
+            <div class="menu-toggle">
+                <h3>Menu</h3>
+                <button type="button" id="menu-btn">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+            <!-- Responsive Menu Structure-->
+            <!--Note: declare the Menu style in the data-menu-style="horizontal" (options: horizontal, vertical, accordion) -->
+            <ul id="respMenu" class="ace-responsive-menu" data-menu-style="horizontal">
+                <li>
+                    <a href="{{ route('home') }}">
+                        Home
+                    </a>
+                </li>
+                <li>
+                    <a>
+                        Company
+                        <span class="arrow"></span> 
+                    </a>
+                    <ul>
+                        <li><a href="@{{ route('about-oec') }}">About OEC</a></li>
+                        <li><a href="@{{ route('careers') }}">Careers</a></li>
+                        <li><a href="@{{ route('sustainability') }}">Sustainability</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a>
+                        Products
+                        <span class="arrow"></span> 
+                    </a>
+                    <ul>
+                        <li><a href="@{{ route('electricals') }}">Electricals</a></li>
+                        <li><a href="@{{ route('automotive') }}">Automotive</a></li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="@{{ route('markets') }}">
+                        Markets
+                    </a>
+                </li>
+                <li>
+                    <a href="@{{ route('login') }}">
+                        Login/Register
+                    </a>
+                </li>
+            </ul>
+        </nav>
+    </div>
 </div>
 </header>
 <!-- End of Responsive Menu -->
