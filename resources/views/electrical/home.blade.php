1453b8b1bef9afe911836b92316f5ad0b0126712
Nikhil <nikhilgoku8@gmail.com>
2025-07-09 13:08:23 +0530

    Banner added

--- resources/views/electrical/home.blade.php
+++ resources/views/electrical/home.blade.php
@@ -2,14 +2,13 @@
 
 @section('content')
 
-<!-- <div class="hero">
+<div class="hero">
     <div class="video_wrapper">
-        <video autoplay muted loop poster="{{ asset('front/assets/images/oec-videoframe.webp') }}">
-            <source src="{{ asset('front/assets/videos/OEC.mp4') }}" type="video/mp4">
+        <video autoplay muted loop poster="https://placehold.co/1920x900/?text=Slider">
+            <source src="" type="video/mp4">
         </video>
-        <div class="overlay_text wow zoomIn" data-wow-delay="0.1s">LEADING MANUFACTURER OF <br> ELECTRICAL AND AUTOMOTIVE SOLUTIONS</div>
     </div>
-</div> -->
+</div>
 
 <div class="our_brands">
     <!-- <div class="container"> -->
